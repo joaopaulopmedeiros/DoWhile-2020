aea7019843b8cc585814c77628750549f871fae7
João Paulo Pereira de Medeiros <medeiros.p@academico.ifrn.edu.br>
2020-12-14 15:33:24 -0300

    Application models

---
+++ src/models/CategorySchema.ts
@@ -0,0 +1,14 @@
+import * as mongoose from "mongoose";
+
+const Schema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Categories", Schema);
